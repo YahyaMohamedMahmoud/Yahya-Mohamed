9b0f2903f534869b766b89755b271239964b3cbf
YahyaMohamedMahmoud <151754506+YahyaMohamedMahmoud@users.noreply.github.com>
2025-06-10 20:03:40 +0300

    update commit

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  distDir: 'build',
   trailingSlash: true,
+  // بدون أي output أو distDir
 };
 
 export default nextConfig;
